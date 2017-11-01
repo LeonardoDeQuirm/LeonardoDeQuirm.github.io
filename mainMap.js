de77cd3f0f2c545385bc09d5d041fcd14471a05b
LeonardoDeQuirm <doctordubin4396+github@gmail.com>
2017-11-01 16:57:55 -0400

    Added favicon (possible placeholder)

--- mainMap.js
+++ mainMap.js
@@ -1,3 +1,23 @@
 "use strict";
 
 //Global variables
+var map;
+
+function renderMyMap() {
+    map = L.map('map').setView([37.8, -96], 4);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.light'
+    }).addTo(map);
+
+    map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
+}
+
+window.onload = function () {
+    renderMyMap();
+};
